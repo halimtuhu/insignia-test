af2396636a9c2906d05150b544ce6d4ac049e1e7
halimtuhu <halimtuhuprasetyo@gmail.com>
2024-02-18 05:44:30 +0700

    feat: :sparkles: add docker

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT || 3000;
+  const port = process.env.API_PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
