7359471d1289490ff01da9faf45b558a169e7e1b
halimtuhu <halimtuhuprasetyo@gmail.com>
2024-02-19 17:38:53 +0700

    feat: :lipstick: adjust channel section responsive

--- apps/social-network/src/components/home/channel-section.tsx
+++ apps/social-network/src/components/home/channel-section.tsx
@@ -5,37 +5,44 @@ import { HTMLAttributes } from 'react';
 import { cn } from '../../helpers/class-helper';
 
 export function ChannelSection({ className }: HTMLAttributes<HTMLDivElement>) {
+  const channels = [
+    { name: 'Google' },
+    { name: 'Dribble' },
+    { name: 'Microsoft' },
+    { name: 'Behance' },
+    { name: 'Weather Channel' },
+    { name: 'Gurus' },
+    { name: 'iMedia' },
+    { name: 'Creativeye' },
+    { name: 'Khan Studios' },
+    { name: 'Yahoo' },
+  ];
+
   return (
     <div className={cn('px-4', className)}>
       <div className="mb-3 flex items-center justify-between">
-        <h3 className="font-semibold">Channels</h3>
+        <h3 className="font-semibold md:text-lg">Channels</h3>
         <Link href="#" className="flex items-center gap-2">
           <span>Browse all channel</span>
           <IconsArrow direction="right" className="w-h4 h-4" />
         </Link>
       </div>
-      <div className="grid grid-cols-3 gap-3">
-        <div className="bg-accent text-primary flex items-start justify-center rounded-sm p-5">
-          <h3 className="font-semibold">Google</h3>
-        </div>
-        <div className="bg-accent text-primary before:border-t-primary relative flex items-start justify-center rounded-sm p-5 before:absolute before:right-0 before:top-0 before:h-0 before:w-0 before:border-l-[2rem] before:border-t-[2rem] before:border-l-transparent before:content-['']">
-          <h3 className="font-semibold">Dribble</h3>
-          <button className="text-accent absolute right-0 top-0 mr-0.5 mt-0.5">
-            <IconsX className="h-4 w-4" />
-          </button>
-        </div>
-        <div className="bg-accent text-primary flex items-start justify-center rounded-sm p-5">
-          <h3 className="font-semibold">Youtube</h3>
-        </div>
-        <div className="bg-accent text-primary flex items-start justify-center rounded-sm p-5">
-          <h3 className="font-semibold">Google</h3>
-        </div>
-        <div className="bg-accent text-primary flex items-start justify-center rounded-sm p-5">
-          <h3 className="font-semibold">Dribble</h3>
-        </div>
-        <div className="bg-accent text-primary flex items-start justify-center rounded-sm p-5">
-          <h3 className="font-semibold">Youtube</h3>
-        </div>
+
+      <div className="grid grid-cols-3 gap-3 md:grid-cols-2">
+        {channels.map((channel, i) => (
+          <div
+            key={i}
+            className={cn(
+              'bg-accent text-primary relative flex aspect-[3/2] items-start justify-center rounded-sm p-2 md:items-end md:justify-start',
+              "hover:before:border-t-primary/90 hover:before:absolute hover:before:right-0 hover:before:top-0 hover:before:h-0 hover:before:w-0 hover:before:border-l-[2rem] hover:before:border-t-[2rem] hover:before:border-l-transparent hover:before:transition-all hover:before:duration-75 hover:before:ease-in-out hover:before:content-['']"
+            )}
+          >
+            <h3 className="font-semibold">{channel.name}</h3>
+            <button className="text-accent absolute right-0 top-0 mr-0.5 mt-0.5">
+              <IconsX className="h-4 w-4" />
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
