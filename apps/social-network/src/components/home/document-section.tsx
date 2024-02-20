d0e64f716bbafaa62647f81c0138538a27115889
halimtuhu <halimtuhuprasetyo@gmail.com>
2024-02-20 14:08:03 +0700

    feat: :sparkles: add skeleton to document section

--- apps/social-network/src/components/home/document-section.tsx
+++ apps/social-network/src/components/home/document-section.tsx
@@ -9,14 +9,17 @@ import { PostPreview } from '../../helpers/interfaces';
 import { api } from '../../helpers/api';
 import { getPaginationParams } from '../../helpers/common';
 import Image from 'next/image';
+import { IconsImage } from '../icons/icons-image';
 
 export function DocumentSection({ className }: HTMLAttributes<HTMLDivElement>) {
   const [documents, setDocuments] = useState<PostPreview[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    api
-      .getPosts(getPaginationParams(1, 5))
-      .then((res) => setDocuments(res.data));
+    api.getPosts(getPaginationParams(1, 5)).then((res) => {
+      setDocuments(res.data);
+      setLoading(false);
+    });
   });
 
   return (
@@ -30,26 +33,43 @@ export function DocumentSection({ className }: HTMLAttributes<HTMLDivElement>) {
       </div>
 
       <div className="-mx-4 flex items-center gap-3 overflow-x-auto px-4 md:mx-0 md:grid md:grid-cols-3 md:grid-rows-3 md:px-0">
-        {documents.map((document, i) => (
-          <div
-            key={i}
-            className="bg-accent text-primary relative flex aspect-[5/6] w-[45%] flex-none flex-col items-start justify-end rounded-sm p-3 first:col-span-2 first:row-span-2 md:aspect-[4/3] md:h-full md:w-full"
-          >
-            <Image
-              className="absolute inset-0 h-full w-full object-cover object-center opacity-25"
-              fill
-              sizes="100%"
-              src={document.image}
-              alt={document.text}
-            />
-            <h3 className="relative text-sm font-semibold">{document.text}</h3>
-            <div className="relative text-sm">{document.tags.join(' ')}</div>
-            <hr className="bg-primary relative my-1 h-0.5 w-4" />
-            <div className="relative text-xs font-semibold">
-              {document.likes} views
-            </div>
-          </div>
-        ))}
+        {!loading
+          ? documents.map((document, i) => (
+              <div
+                key={i}
+                className="bg-accent text-primary relative flex aspect-[5/6] w-[45%] flex-none flex-col items-start justify-end rounded-sm p-3 first:col-span-2 first:row-span-2 md:aspect-[4/3] md:h-full md:w-full"
+              >
+                <Image
+                  className="absolute inset-0 h-full w-full object-cover object-center opacity-25"
+                  fill
+                  sizes="100%"
+                  src={document.image}
+                  alt={document.text}
+                />
+                <h3 className="relative text-sm font-semibold">
+                  {document.text}
+                </h3>
+                <div className="relative text-sm">
+                  {document.tags.join(' ')}
+                </div>
+                <hr className="bg-primary relative my-1 h-0.5 w-4" />
+                <div className="relative text-xs font-semibold">
+                  {document.likes} views
+                </div>
+              </div>
+            ))
+          : Array.from(Array(5)).map((_, i) => (
+              <div
+                key={i}
+                className="bg-accent text-primary relative flex aspect-[5/6] w-[45%] flex-none flex-col items-start justify-end gap-2 rounded-sm p-3 first:col-span-2 first:row-span-2 md:aspect-[4/3] md:h-full md:w-full"
+              >
+                <IconsImage className="absolute inset-0 m-auto h-12 w-12 animate-pulse" />
+                <h3 className="bg-primary relative inline-block h-4 w-2/3 animate-pulse rounded-sm font-semibold md:text-lg"></h3>
+                <div className="bg-primary relative h-2 w-1/3 animate-pulse rounded-sm text-sm"></div>
+                <hr className="bg-primary relative my-1 h-0.5 w-4" />
+                <div className="bg-primary relative h-2 w-1/6 animate-pulse rounded-sm text-xs font-semibold"></div>
+              </div>
+            ))}
         <button className="bg-primary text-accent border-accent hidden aspect-video h-full w-full flex-none items-center gap-2 rounded-sm border p-8 md:flex">
           <IconsCirclePlus className="h-10 w-10" direction="up" />
           <div className="text-left">
